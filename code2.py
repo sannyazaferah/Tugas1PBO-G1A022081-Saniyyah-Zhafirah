68291c0fa3013c807b4f516d0e4f6786e69c2484
sannyazaferah <131683284+sannyazaferah@users.noreply.github.com>
2023-04-24 06:43:58 -0800

    Create code2.py

---
+++ code2.py
@@ -0,0 +1,7 @@
+class pakaian:
+  def__init__(self, jenis, ukuran):
+     print("pakaian saya yaitu")
+     self.jenis = jenis
+     self.ukuran = ukuran
+pakaian1 = pakaian('kemeja', 'medium')
+pakaian2 = pakaian('kaos', 'oversize')
